f7c50343e973d114731da18aa8428dff958a3b94
vaseeece <vaseeece@gmail.com>
2021-02-10 23:11:47 +0530

    code format

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const config = require('./config.json');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const config = require("./config.json");
 const port = 4400;
-const url = `mongodb+srv://${config.mongodb.userName}:${config.mongodb.password}${config.mongodb.clusterDetails}/${config.mongodb.dbName}?retryWrites=true&w=majority`;
+const url = `mongodb+srv://${config.mongodb.userName}:${config.mongodb.password}@${config.mongodb.clusterDetails}/${config.mongodb.dbName}?retryWrites=true&w=majority`;
 
 const app = express();
 app.use(cors());
@@ -11,15 +11,15 @@ app.use(cors());
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 const con = mongoose.connection;
 
-con.on('open', () => {
-    console.log('DB connected...');
+con.on("open", () => {
+  console.log("DB connected...");
 });
 
-app.use(express.json())
+app.use(express.json());
 
-const participantsRouter = require('./routes/participants');
-app.use('/participants', participantsRouter);
+const participantsRouter = require("./routes/participants");
+app.use("/participants", participantsRouter);
 
 app.listen(port, () => {
-    console.log(`server started and listening to port ${port}...`)
+  console.log(`server started and listening to port ${port}...`);
 });
